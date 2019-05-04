517d179a016ff71b7e29fd8881e9b22217fecf59
JerecTharen <kf7ags@gmail.com>
2019-05-04 13:10:51 -0600

    initial

---
+++ InsertScript.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.users(
+	uid, user_name, first_name, last_name, email, password, profile_picutre, added_recipes_table, saved_recipes_table, friends_table)
+	VALUES (1, {'j','e','r','e','c','t','h','a','r','e','n'}, 'Jacob', 'Bennett', 'password1', 'kf7ags@gmail.com', 'http://myUrl.com/extension/goes/here', 'jerec_tharen_recipes', 'jerec_tharen_saved', 'jerec_tharen_friends');
